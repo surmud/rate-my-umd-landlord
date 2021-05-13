b15564cad092da720889030cb8d7b89f3d869f97
Surmud <surmud@umd.edu>
2021-05-13 01:34:56 -0400

    changed config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/rate-my-umd-landlord"
+#MONGODB_HOST = "mongodb://localhost:27017/rate-my-umd-landlord"
